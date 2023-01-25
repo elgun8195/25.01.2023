032351d04c23e6a999513119b26589aee5a634d4
elgun8195 <tu741rmps@code.edu.az>
2023-01-25 20:32:57 +0400

    inital commits

---
+++ Boake-BackEnd/ViewModels/BookDetailVM.cs
@@ -0,0 +1,13 @@
+﻿using Boake_BackEnd.Models;
+using System.Collections.Generic;
+
+namespace Boake_BackEnd.ViewModels
+{
+    public class BookDetailVM
+    {
+        public Book Book { get; set; }
+        public List<Book> RelatedBooks { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
